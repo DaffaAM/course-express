5c386e8bfb1fdace343c32028b158ad1544b9673
Daffa Abdhy Muzhaffar <45245291+DaffaAM@users.noreply.github.com>
2020-02-26 14:00:06 +0700

    deploy test

--- index.js
+++ index.js
@@ -1,42 +1,38 @@
 // instatiate express module
-const express = require('express')
-require('express-group-routes')
-
-
+const express = require("express");
+require("express-group-routes");
 
 //use express in app variable
-const app = express()
+const app = express();
 
 //init bodyParser
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 
 //define the server port
-const port = 3500
+const port = process.env.PORT || 5000;
 
 //allow this app to receive incoming json request
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 //create the homepage root
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   //res means response, and it send string "Hello Express!" to the API
-  res.send('Hello Express!')
-})
+  res.send("Hello Express!");
+});
 
 //when this nodejs app executed, it will listen to defined port
 // app.listen(port, () => console.log(`Listening on port ${port}!`))
 
 //make hardcoded array of obj todos
 //import the controller
-const TodosController = require('./controllers/todos')
-const { authenticated } = require('./middleware')
-const AuthController = require('./controllers/auth')
-
+const TodosController = require("./controllers/todos");
+const { authenticated } = require("./middleware");
+const AuthController = require("./controllers/auth");
 
 //GET list route: simply send arr of obj todos your user screen
-app.group('/api/v1', (router) => {
-
-  router.post('/login', AuthController.login)
-  router.post('/register', AuthController.signUp)
+app.group("/api/v1", router => {
+  router.post("/login", AuthController.login);
+  router.post("/register", AuthController.signUp);
   //
   // router.get('/todos', authenticated, TodosController.index)
   //
@@ -52,7 +48,6 @@ app.group('/api/v1', (router) => {
   //
   // //DELETE route: delete the todo obj, by received id request paramas
   // router.delete('/todo/:id', authenticated, TodosController.destroy)
+});
 
-})
-
-app.listen(port, () => console.log(`Listening on port ${port}!`))
+app.listen(port, () => console.log(`Listening on port ${port}!`));
